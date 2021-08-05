426c112a05bfa1bc56e5cddd7cc8f0bcf0872a01
benny <shenbinsc@qq.com>
2021-08-06 00:20:23 +0800

    tested to be working

--- client.js
+++ client.js
@@ -4,12 +4,12 @@ const debug = require('debug')('http')
 require('debug').enable('http');
 
 const fcm = axios.create({
-    baseURL: 'https://fcm.googleapis.com/fcm/send'
+    baseURL: 'https://requestbin.net/r/en4w1rz2'
 })
 
-fcm.defaults.headers.common['Authorization'] = "AAAA_Vt-h2U:APA91bH9KJRvVL0XL8vxdyAimE1gX_O-GVd4AQnrwFx3Tb6tY0_g9E33pcbaw6MjFpz9Yzt8FOmuD43TAKjjV-Kq0nh9PoXSeyFYCK0hHbLvZv2u8dNbjGL8TWNK8vXmbLzdw-4gQJVb"
+fcm.defaults.headers.common['Authorization'] = "some keys"
 
-fcm.post('/', {
+fcm.post('', {
     registration_ids: ['dqZn1Za9Qf6ekVbHobyQDe:APA91bE7rXBiDLxToza_S147MbPRJOTXKfIBDtou5jl1YnfJspA9FhYmketoIkrQkHekvkbjCUnot5zThTri0OKEPvJFt3WBSPMla6kaKzNBsxMJYIFINi9rouJQhEUO1tgOOhJY9rF0'],
     notification: {
         title: "fcm test",
--- index.js
+++ index.js
@@ -8,24 +8,10 @@ require('debug').enable('http');
 
 
 const fcm = axios.create({
-    baseURL: 'https://fcm.googleapis.com/'
+    baseURL: 'https://fcm.googleapis.com/fcm/send'
 })
 
-fcm.defaults.headers.common['Authorization'] = `key=AAAA_Vt-h2U:APA91bH9KJRvVL0XL8vxdyAimE1gX_O-GVd4AQnrwFx3Tb6tY0_g9E33pcbaw6MjFpz9Yzt8FOmuD43TAKjjV-Kq0nh9PoXSeyFYCK0hHbLvZv2u8dNbjGL8TWNK8vXmbLzdw-4gQJVb`
-
-fcm.post('/fcm/send', {
-    registration_ids: ['dqZn1Za9Qf6ekVbHobyQDe:APA91bE7rXBiDLxToza_S147MbPRJOTXKfIBDtou5jl1YnfJspA9FhYmketoIkrQkHekvkbjCUnot5zThTri0OKEPvJFt3WBSPMla6kaKzNBsxMJYIFINi9rouJQhEUO1tgOOhJY9rF0'],
-    notification: {
-        title: "fcm test",
-        body: 'fcm body'
-    }
-})
-.then((res) => {
-    debug('%s', res.status);
-})
-.catch((err) => {
-    debug('%o', err.response);
-})
+fcm.defaults.headers.common['Authorization'] = 'key=AAAA_Vt-h2U:APA91bH9KJRvVL0XL8vxdyAimE1gX_O-GVd4AQnrwFx3Tb6tY0_g9E33pcbaw6MjFpz9Yzt8FOmuD43TAKjjV-Kq0nh9PoXSeyFYCK0hHbLvZv2u8dNbjGL8TWNK8vXmbLzdw-4gQJVb'
 
 
 const app = express();
@@ -35,12 +21,24 @@ app.use(express.json());
 app.use(volleyball)
 
 app.post('/', (req, res) => {
-    res.json({error: 0, message: 'weldone'})
+    res.json({error: 0, message: 'well done!'})
 })
 
 
 app.get('/fcm/send', (req, res) => {
-
+    fcm.post('', {
+        registration_ids: ['dqZn1Za9Qf6ekVbHobyQDe:APA91bE7rXBiDLxToza_S147MbPRJOTXKfIBDtou5jl1YnfJspA9FhYmketoIkrQkHekvkbjCUnot5zThTri0OKEPvJFt3WBSPMla6kaKzNBsxMJYIFINi9rouJQhEUO1tgOOhJY9rF0'],
+        notification: {
+            title: "FCM Test Message",
+            body: `Message sent @ ${new Date().toString()}`
+        }
+    })
+    .then((resp) => {
+        res.sendStatus(200);
+    })
+    .catch((err) => {
+        res.sendStatus(err.response.status)
+    })
 })
 
 app.post('/upload/record', (req, res) => {
