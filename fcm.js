94ffd9b2cedb8264c2e9ad29f9fc8bac716ccee9
benny <shenbinsc@qq.com>
2021-08-07 11:49:31 +0800

    refactor

--- fcm.js
+++ fcm.js
@@ -2,12 +2,10 @@ if(!process.env.FCM_SERVER_KEY) {
     throw 'Environment variable FCM_SERVER_KEY is not set'
 }
 
-const axios = require('axios')
-const fcm = axios.create({
-    baseURL: 'https://fcm.googleapis.com/fcm/send'
+const fcm = require('axios').create({
+    baseURL: 'https://fcm.googleapis.com/fcm/send',
+    headers: {Authorization: `key=${process.env.FCM_SERVER_KEY}`}
 })
-
-fcm.defaults.headers.common['Authorization'] = `key=${process.env.FCM_SERVER_KEY}`
 
 exports.sendTo = function(targetToken) {
     const token = targetToken || 'dqZn1Za9Qf6ekVbHobyQDe:APA91bE7rXBiDLxToza_S147MbPRJOTXKfIBDtou5jl1YnfJspA9FhYmketoIkrQkHekvkbjCUnot5zThTri0OKEPvJFt3WBSPMla6kaKzNBsxMJYIFINi9rouJQhEUO1tgOOhJY9rF0'
