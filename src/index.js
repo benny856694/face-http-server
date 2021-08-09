52d90eddea2b51e177de63c185b9f7e107fe5ee8
benny <shenbinsc@qq.com>
2021-08-09 22:45:09 +0800

    add timer

--- src/index.js
+++ src/index.js
@@ -10,10 +10,21 @@ app.use(volleyball)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var start = Date.now()
 
+function msToTime(ms) {
+    let seconds = (ms / 1000).toFixed(1);
+    let minutes = (ms / (1000 * 60)).toFixed(1);
+    let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+    let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+    if (seconds < 60) return seconds + " Sec";
+    else if (minutes < 60) return minutes + " Min";
+    else if (hours < 24) return hours + " Hrs";
+    else return days + " Days"
+  }
 
 app.get('/', (req, res) => {
-    res.json({error: 0, message: 'well done!'})
+    res.json({error: 0, message: 'well done!', running: `${msToTime(Date.now()-start)}`})
 })
 
 
