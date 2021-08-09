a584a8023475001c313a64f13d6886d58abafaff
benny <shenbinsc@qq.com>
2021-08-09 19:21:14 +0800

    add log to file

--- src/fcm.js
+++ src/fcm.js
@@ -2,6 +2,36 @@ const fs = require('fs')
 const debug = require('debug')('fcm')
 const {JWT} = require('google-auth-library')
 
+
+
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: './log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './log/combined.log' }),
+  ],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+
+
 var keys = null
 var fcm = null
 
@@ -14,7 +44,7 @@ async function getToken() {
     debug('%s', keyFile)
     buffer = fs.readFileSync(keyFile)
     keys = JSON.parse(buffer)
-    debug('%O', keys)
+    logger.info(keys)
     const client = new JWT(
       keys.client_email,
       null,
