7b260db314a3c04b4657232175c744a77934790a
benny <shenbinsc@gmail.com>
2022-07-11 17:06:26 +0800

    add fake server

---
+++ src/fake_server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const debug = require('debug')('http');
+const volleyball = require('volleyball')
+
+
+var servercfg = require('./server_config.json');
+
+
+const app = express();
+app.use(volleyball)
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+for (const s in servercfg) {
+    if (Object.hasOwnProperty.call(servercfg, s)) {
+        const server = servercfg[s];
+        //debug(server);
+        for (const url in server) {
+            if (Object.hasOwnProperty.call(server, url)) {
+                const element = server[url];
+                debug(element.url, element.data)
+                app.post(element.url, (req, res) => {
+                    res.send(element.data)
+                } )    
+            }
+        }
+       
+    }
+}
+
+
+const port = 5000;
+app.listen(port, () => debug(`server started at ${port}`));
+
